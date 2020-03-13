8a92afa557b9af295c0d593cba85d74bff63c503
Justin Haze <jhaze@ntia.doc.gov>
2020-03-13 12:48:35 -0600

    Updated setup.py dependencies and metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import os
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+repo_root = os.path.dirname(os.path.realpath(__file__))
+requirements_path = repo_root + '/requirements.txt'
+install_requires = []  # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
 
 
 setuptools.setup(
@@ -10,9 +20,9 @@ setuptools.setup(
     author="The Institute for Telecommunication Sciences",
     # author_email="author@example.com",
     description="Base actions library for scos-sensor",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    #url="https://github.com/pypa/sampleproject",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NTIA/scos_actions",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,4 +30,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=install_requires
 )
