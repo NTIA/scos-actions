7faea0cca85015a2748b37ae40893dd6ae41724a
Justin Haze <jhaze@ntia.doc.gov>
2021-04-05 15:15:33 -0600

    remove admin_only

--- scos_actions/actions/sync_gps.py
+++ scos_actions/actions/sync_gps.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 class SyncGps(Action):
     """Query the GPS and synchronize time and location."""
 
-    def __init__(self, gps, admin_only=True):
-        super(SyncGps, self).__init__(admin_only=admin_only)
+    def __init__(self, gps):
+        super(SyncGps, self).__init__()
 
         self.gps = gps
 
