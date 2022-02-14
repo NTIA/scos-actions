129d6d49415b95776e499e1102c877be4e2c2fbb
dboulware <dboulware@ntia.doc.gov>
2022-02-14 13:58:09 -0700

    Moved preselector_config.json to scos-sensor so it can be deployed by puppet. Made preselector type configurable.

--- scos_actions/hardware/__init__.py
+++ scos_actions/hardware/__init__.py
@@ -1,14 +1,18 @@
 from scos_actions.hardware.mocks.mock_gps import MockGPS
 from scos_actions.hardware.mocks.mock_radio import MockRadio
 from scos_actions.capabilities import capabilities
-from scos_actions.settings import PRESELECTOR_CONFIG_FILE
+from scos_actions.settings import PRESELECTOR_CONFIG_FILE\
+from scos_actions.settings import PRESELECTOR_MODULE
+from scos_actions.settings import PRESELECTOR_CLASS
 from scos_actions import utils
-from its_preselector.preselector import Preselector
+import importlib
 
 
 def load_preselector(preselector_config_file):
     preselector_config = utils.load_from_json(preselector_config_file)
-    preselector = Preselector(capabilities, preselector_config)
+    preselector_module = importlib.import_module(PRESELECTOR_MODULE)
+    preselector_class = getattr(preselector_module, PRESELECTOR_CLASS)
+    preselector = preselector_class(capabilities, preselector_config)
     return preselector
 
 
