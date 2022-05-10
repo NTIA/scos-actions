d70f278f468265b4ebbfc1e19a2f7a8e786655c9
Doug Boulware <dboulware@ntia.doc.gov>
2022-05-10 12:27:43 -0600

    print cal values

--- scos_actions/actions/calibrate_y_factor.py
+++ scos_actions/actions/calibrate_y_factor.py
@@ -165,10 +165,10 @@ class YFactorCalibration(SingleFrequencyFftAcquisition):
         window = windows.flattop(self.parameters[FFT_SIZE])
         enbw = get_enbw(window, self.parameters[SAMPLE_RATE])
         enr = self.get_enr()
-        logger.debug('ENR: ' + str(enr))
+        logger.info('ENR: ' + str(enr))
         noise_figure, gain = y_factor(mean_on_watts, mean_off_watts, enr, enbw)
-        logger.debug('Noise Figure:' + str(noise_figure))
-        logger.debug('Gain: ' + str(gain))
+        logger.info('Noise Figure:' + str(noise_figure))
+        logger.info('Gain: ' + str(gain))
         sensor_calibration.update(params,utils.get_datetime_str_now(), gain, noise_figure, SENSOR_CALIBRATION_FILE)
 
     def get_enr(self):
