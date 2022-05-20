8a416729406d2b91a070beeeae480b129af05392
Doug Boulware <dboulware@ntia.doc.gov>
2022-05-20 14:52:08 -0600

    set name in measurement_result in fft action.

--- scos_actions/actions/acquire_single_freq_fft.py
+++ scos_actions/actions/acquire_single_freq_fft.py
@@ -152,6 +152,7 @@ n
         measurement_result['window'] = 'flattop'
         measurement_result['calibration_datetime'] = self.sigan.sensor_calibration_data['calibration_datetime']
         measurement_result['description'] = self.description
+        measurement_result['name'] = self.parameter_map['name']
         self.add_metadata_decorators(measurement_result)
         self.create_metadata(schedule_entry_json, measurement_result)
         measurement_action_completed.send(
