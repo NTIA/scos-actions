98f972c8cf8658eff741c7a86e8e098d8e99ac46
Anthony Romaniello <aromaniello@ntia.gov>
2022-07-07 12:49:50 -0600

    Merge upstream default calibration change

--- scos_actions/hardware/sigan_iface.py
+++ scos_actions/hardware/sigan_iface.py
@@ -2,7 +2,10 @@ import copy
 from abc import ABC, abstractmethod
 from scos_actions.settings import sensor_calibration
 from scos_actions.settings import sigan_calibration
-from scos_actions import utils
+from scos_actions.utils import (
+    convert_string_to_millisecond_iso_format,
+    get_datetime_str_now
+)
 
 
 class SignalAnalyzerInterface(ABC):
@@ -14,6 +17,7 @@ class SignalAnalyzerInterface(ABC):
             "enbw_sigan": None,  # Defaults to sample rate
             "noise_figure_sigan": 0,
             "1db_compression_sigan": 100,
+            "calibration_datetime": get_datetime_str_now()
         }
 
         self.DEFAULT_SENSOR_CALIBRATION = {
@@ -24,6 +28,7 @@ class SignalAnalyzerInterface(ABC):
             "gain_preselector": 0,
             "noise_figure_preselector": 0,
             "1db_compression_preselector": 100,
+            "calibration_datetime": get_datetime_str_now()
         }
         self.sensor_calibration_data = copy.deepcopy(self.DEFAULT_SENSOR_CALIBRATION)
         self.sigan_calibration_data = copy.deepcopy(self.DEFAULT_SIGAN_CALIBRATION)
@@ -31,8 +36,8 @@ class SignalAnalyzerInterface(ABC):
     @property
     def last_calibration_time(self):
         """Returns the last calibration time from calibration data."""
-        return utils.convert_string_to_millisecond_iso_format(
-                sensor_calibration.calibration_datetime
+        return convert_string_to_millisecond_iso_format(
+            sensor_calibration.calibration_datetime
         )
 
     @property
@@ -79,6 +84,3 @@ class SignalAnalyzerInterface(ABC):
             self.sigan_calibration_data.update(
                     sigan_calibration.get_calibration_dict(cal_args)
             )
-
-
-    
