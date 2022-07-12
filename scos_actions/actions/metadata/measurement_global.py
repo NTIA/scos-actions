3054f8c8641cdf476a319d531fe4fe7a921e5209
Anthony Romaniello <aromaniello@ntia.gov>
2022-07-12 16:35:56 -0600

    Initialized frequencies as None to avoid errors

--- scos_actions/actions/metadata/measurement_global.py
+++ scos_actions/actions/metadata/measurement_global.py
@@ -7,14 +7,15 @@ class MeasurementMetadata(Metadata):
     def __init__(self):
         super().__init__()
 
-    def create_metadata(self,  sigmf_builder: SigMFBuilder, measurement_result: dict):
+    def create_metadata(self, sigmf_builder: SigMFBuilder, measurement_result: dict):
+        freq_low = None
+        freq_high = None
 
         if 'frequency_low' in measurement_result:
             freq_low = measurement_result['frequency_low']
         elif 'frequency' in measurement_result:
             freq_low = measurement_result['frequency']
             freq_high = measurement_result['frequency']
-
         if 'frequency_high' in measurement_result:
             freq_high = measurement_result['frequency_high']
 
