7170d59a2c3e5341c035be80ccc2b4506c7f67f9
Anthony Romaniello <aromaniello@ntia.gov>
2022-07-12 12:39:46 -0600

    Added temperature conversions

--- scos_actions/signal_processing/unit_conversion.py
+++ scos_actions/signal_processing/unit_conversion.py
@@ -20,13 +20,13 @@ def convert_watts_to_dBm(
     """
     Convert from Watts to dBm.
 
-    Calculation: 10 * log10(val_watts) + 30
+    Calculation: ``10 * log10(val_watts) + 30``
 
     NumPy is used for scalar inputs.
     NumExpr is used to speed up the operation for arrays.
 
     :param val_watts: A value, or array of values, in Watts.
-    :returns: The input val_watts, converted to dBm.
+    :return: The input val_watts, converted to dBm.
     """
     suppress_divide_by_zero_when_testing()
     if np.isscalar(val_watts):
@@ -40,13 +40,13 @@ def convert_dBm_to_watts(val_dBm: Union[float, np.ndarray]) -> Union[float, np.n
     """
     Convert from dBm to Watts.
 
-    Calculation: 10^((val_dBm - 30) / 10)
+    Calculation: ``10^((val_dBm - 30) / 10)``
 
     NumPy is used for scalar inputs.
     NumExpr is used to speed up the operation for arrays.
 
     :param val_dBm: A value, or array of values, in dBm.
-    :returns: The input val_dBm, converted to Watts.
+    :return: The input val_dBm, converted to Watts.
     """
     if np.isscalar(val_dBm):
         val_watts = 10.0 ** ((val_dBm - 30) / 10)
@@ -61,14 +61,14 @@ def convert_linear_to_dB(
     """
     Convert from linear units to dB.
 
-    Calculation: 10 * log10(val_linear)
+    Calculation: ``10 * log10(val_linear)``
 
     NumPy is used for scalar inputs.
     NumExpr is used to speed up the operation for arrays.
 
     :param val_linear: A value, or array of values, in linear
         units.
-    :returns: The input val_linear, convert to dB.
+    :return: The input val_linear, converted to dB.
     """
     suppress_divide_by_zero_when_testing()
     if np.isscalar(val_linear):
@@ -82,16 +82,64 @@ def convert_dB_to_linear(val_dB: Union[float, np.ndarray]) -> Union[float, np.nd
     """
     Convert from dB to linear units.
 
-    Calculation: 10^(val_dB / 10)
+    Calculation: ``10^(val_dB / 10)``
 
     NumPy is used for scalar inputs.
     NumExpr is used to speed up the operation for arrays.
 
     :param val_dB: A value, or array of values, in dB.
-    :returns: The input val_dB, in corresponding linear units.
+    :return: The input val_dB, converted to corresponding
+        linear units.
     """
     if np.isscalar(val_dB):
         val_linear = 10.0 ** (val_dB / 10.0)
     else:
         val_linear = ne.evaluate("10**(val_dB/10)")
     return val_linear
+
+
+def convert_kelvins_to_celsius(
+    val_kelvins: Union[float, np.ndarray]
+) -> Union[float, np.ndarray]:
+    """
+    Convert from Kelvins to degrees Celsius.
+
+    Calculation: ``val_kelvins - 273.15``
+
+    :param val_kelvins: A value, or array of values, in
+        Kelvins.
+    :return: The input val_kelvins, converted to degrees
+        Celsius.
+    """
+    return val_kelvins - 273.15
+
+
+def convert_celsius_to_kelvins(
+    val_celsius: Union[float, np.ndarray]
+) -> Union[float, np.ndarray]:
+    """
+    Convert from degrees Celsius to Kelvins.
+
+    Calculation: ``val_celsius + 273.15``
+
+    :param val_celsius: A value, or array of values, in
+        degrees Celsius.
+    :return: The input val_celsius, converted to Kelvins.
+    """
+    return val_celsius + 273.15
+
+
+def convert_fahrenheit_to_celsius(
+    val_fahrenheit: Union[float, np.ndarray]
+) -> Union[float, np.ndarray]:
+    """
+    Convert from degrees Fahrenheit to degrees Celsius.
+
+    Calculation: ``(val_fahrenheit - 32) * (5 / 9)``
+
+    :param val_fahrenheit: A value, or array of values, in
+        degrees Fahrenheit.
+    :return: The input val_fahrenheit, converted to degrees
+        Celsius.
+    """
+    return (val_fahrenheit - 32.0) * (5.0 / 9.0)
