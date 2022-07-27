b6d0ee6ef26fb689037e043e3df4e1ce096760f5
Anthony Romaniello <aromaniello@ntia.gov>
2022-07-27 17:23:39 -0600

    Testing scaling

--- scos_actions/actions/calibrate_y_factor.py
+++ scos_actions/actions/calibrate_y_factor.py
@@ -240,10 +240,10 @@ class YFactorCalibration(Action):
             num_ffts=nffts,
             shift=True
         )
-        complex_fft /= 2  # INCORRECT SCALING for testing
+        # TESTING SCALING
         complex_fft *= fft_window_cf
         power_fft = calculate_power_watts(complex_fft)
-        # power_fft /= 2  # RF/baseband conversion
+        power_fft /= 2  # RF/baseband conversion
         # power_fft *= fft_window_cf  # Window correction
         mean_result = apply_power_detector(power_fft, self.fft_detector)
         return mean_result
