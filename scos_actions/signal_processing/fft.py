6b06e576273542a7a4e0af47c9a6e63ae8b6c92f
Anthony Romaniello <aromaniello@ntia.gov>
2022-08-02 12:31:19 -0600

    fix typeerror

--- scos_actions/signal_processing/fft.py
+++ scos_actions/signal_processing/fft.py
@@ -85,6 +85,8 @@ def get_fft(
     # Apply the FFT window if provided
     if fft_window is not None:
         logger.debug("Applying window before FFT")
+        if time_data.dtype is np.complex64:
+            time_data = time_data.astype(np.complex128)
         logger.debug(f"Time data: {time_data[0,:5]}, window: {fft_window[:5]}")
         ne.evaluate("time_data*fft_window", out=time_data)
         logger.debug("After windowing:")
