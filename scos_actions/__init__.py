cb19b743d8ce3b1be4d717e43602d117c19e63ce
Anthony Romaniello <aromaniello@ntia.gov>
2022-08-10 11:23:06 -0600

    Switch to flit backend

---
+++ scos_actions/__init__.py
@@ -0,0 +1,5 @@
+"""The base plugin providing common actions and interfaces for SCOS Sensor plugins.
+
+Refer to the SCOS Actions README file for detailed usage information.
+"""
+__version__ = "2.0.0"
