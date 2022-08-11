a91437c3aa2b09e491ef6a78c28f7ad8adba3619
Doug Boulware <dboulware@ntia.doc.gov>
2022-08-11 15:23:22 -0600

    debug

--- scos_actions/settings.py
+++ scos_actions/settings.py
@@ -39,7 +39,7 @@ CONFIG_DIR = path.join(
 ACTION_DEFINITIONS_DIR = path.join(
     path.dirname(path.abspath(__file__)), "configs/actions"
 )
-
+logger.debug("setting Configured: " + str(settings.configured))
 # set sigan_calibration file and sensor_calibration_file
 if not settings.configured or not hasattr(settings, "SIGAN_CALIBRATION_FILE"):
     logger.warning("Using default sigan cal file.")
