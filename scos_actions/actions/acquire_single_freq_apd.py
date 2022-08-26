ea04d5681879fe0b6e64d4b80feae0ed54695c69
Anthony Romaniello <aromaniello@ntia.gov>
2022-08-26 13:40:36 -0600

    Update syntax for upstream changes

--- scos_actions/actions/acquire_single_freq_apd.py
+++ scos_actions/actions/acquire_single_freq_apd.py
@@ -33,7 +33,7 @@ First, the {num_samps} continuous samples are acquired from the signal analyzer.
 specified, a voltage scaling factor is applied to the time-domain signals. The time
 domain complex voltage samples are converted to real-valued amplitudes which are used to
 generate the APD. After the APD is generated, the amplitude values are further converted
-to power values in dBm, with calculations assuming a 50 Ohm system. 
+to power values in dBm, with calculations assuming a 50 Ohm system.
 
 If no downsampling is specified (by either not providing the apd_bin_size_dB parameter, or
 setting its value to be less than or equal to zero), the APD is estimated using the routine
@@ -57,16 +57,17 @@ and amplitude axes of the estimated APD. This should be altered as future update
 provide better ways to store such data.
 """
 import logging
+
 import numexpr as ne
 import numpy as np
-from scos_actions.actions.interfaces.measurement_action import (MeasurementAction)
-from scos_actions.actions.action_utils import get_param, ParameterException
-from scos_actions.actions.metadata.sigmf_builder import Domain, MeasurementType, SigMFBuilder
+
+from scos_actions import utils
+from scos_actions.actions.interfaces.measurement_action import MeasurementAction
+from scos_actions.hardware import gps as mock_gps
+from scos_actions.metadata.sigmf_builder import Domain, MeasurementType, SigMFBuilder
 from scos_actions.signal_processing.apd import get_apd
 from scos_actions.signal_processing.unit_conversion import convert_linear_to_dB
-from scos_actions.hardware import gps as mock_gps
-from scos_actions import utils
-
+from scos_actions.utils import ParameterException, get_parameter
 
 logger = logging.getLogger(__name__)
 
@@ -88,28 +89,31 @@ class SingleFrequencyApdAcquisition(MeasurementAction):
         frequency: center frequency in Hz
         duration_ms: duration to acquire in ms
         apd_bin_size_dB: amplitude resolution of IQ data, reduces data size. defaults to 0.5
-         
+
     For the parameters required by the signal analyzer, see the documentation from the Python
     package for the signal analyzer being used.
 
     :param parameters: Dictionary of parameters needed for the action and signal analyzer.
     :param sigan: instance of SignalAnalyzerInterface
     """
+
     def __init__(self, parameters, sigan, gps=mock_gps):
         super().__init__(parameters, sigan, gps)
         # Pull parameters from action config
-        self.nskip = get_param(NUM_SKIP, self.parameter_map)
-        self.duration_ms = get_param(DURATION_MS, self.parameter_map)
-        self.frequency_Hz = get_param(FREQUENCY, self.parameter_map)
-        self.sample_rate_Hz = get_param(SAMPLE_RATE, self.parameter_map)
+        self.nskip = get_parameter(NUM_SKIP, self.parameters)
+        self.duration_ms = get_parameter(DURATION_MS, self.parameters)
+        self.frequency_Hz = get_parameter(FREQUENCY, self.parameters)
+        self.sample_rate_Hz = get_parameter(SAMPLE_RATE, self.parameters)
         self.num_samples = int(self.sample_rate_Hz * self.duration_ms * 1e-3)
         try:
-            self.apd_bin_size = get_param(APD_BIN_SIZE, self.parameter_map)
+            self.apd_bin_size = get_parameter(APD_BIN_SIZE, self.parameters)
         except ParameterException:
             logger.warning("APD bin size not configured. Using no downsampling.")
             self.apd_bin_size = None
         if self.apd_bin_size <= 0:
-            logger.warning("APD bin size set to zero or negative. Using no downsampling.")
+            logger.warning(
+                "APD bin size set to zero or negative. Using no downsampling."
+            )
             self.apd_bin_size = None
 
     def execute(self, schedule_entry, task_id) -> dict:
@@ -123,15 +127,17 @@ class SingleFrequencyApdAcquisition(MeasurementAction):
         # Save measurement results
         measurement_result["data"] = apd_result
         measurement_result.update(self.parameter_map)
-        measurement_result['start_time'] = start_time
-        measurement_result['end_time'] = utils.get_datetime_str_now()
+        measurement_result["start_time"] = start_time
+        measurement_result["end_time"] = utils.get_datetime_str_now()
         measurement_result["domain"] = Domain.TIME.value
-        measurement_result['measurement_type'] = MeasurementType.SINGLE_FREQUENCY.value
-        measurement_result['description'] = self.description
-        measurement_result['calibration_datetime'] = self.sigan.sensor_calibration_data['calibration_datetime']
-        measurement_result['task_id'] = task_id
-        measurement_result['sigan_cal'] = self.sigan.sigan_calibration_data
-        measurement_result['sensor_cal'] = self.sigan.sensor_calibration_data
+        measurement_result["measurement_type"] = MeasurementType.SINGLE_FREQUENCY.value
+        measurement_result["description"] = self.description
+        measurement_result["calibration_datetime"] = self.sigan.sensor_calibration_data[
+            "calibration_datetime"
+        ]
+        measurement_result["task_id"] = task_id
+        measurement_result["sigan_cal"] = self.sigan.sigan_calibration_data
+        measurement_result["sensor_cal"] = self.sigan.sensor_calibration_data
         return measurement_result
 
     def get_power_apd(self, measurement_result: dict) -> np.ndarray:
@@ -141,10 +147,10 @@ class SingleFrequencyApdAcquisition(MeasurementAction):
         #  a * 2 : dBV --> dB(V^2)
         #  a - impedance_dB : dB(V^2) --> dBW (hard-coded for 50 Ohm systems)
         #  a + 27 : dBW --> dBm (+30), RF/baseband conversion (-3)
-        scale_factor = 27 - convert_linear_to_dB(50.)
+        scale_factor = 27 - convert_linear_to_dB(50.0)
         ne.evaluate("(2*a)+scale_factor", out=a)
         # For now: store probability and amplitude axes in one complex array
-        return p + 1j*a
+        return p + 1j * a
 
     @property
     def description(self):
@@ -166,11 +172,9 @@ class SingleFrequencyApdAcquisition(MeasurementAction):
         # __doc__ refers to the module docstring at the top of the file
         return __doc__.format(**definitions)
 
-
     def get_sigmf_builder(self, measurement_result: dict) -> SigMFBuilder:
         # TO DO
         return super().get_sigmf_builder(measurement_result)
-
 
     def is_complex(self) -> bool:
         # True for now; should likely change with metadata handling updates
