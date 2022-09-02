0350f0eda63a5e5cf0e4f0fff7a8c89f5d040633
Anthony Romaniello <aromaniello@ntia.gov>
2022-09-01 20:07:58 -0600

    Make calibration keys optional as in spec

--- scos_actions/metadata/annotations/calibration_annotation.py
+++ scos_actions/metadata/annotations/calibration_annotation.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Any, Optional
 
 from scos_actions.metadata.annotation_segment import AnnotationSegment
 
@@ -11,7 +11,7 @@ class CalibrationAnnotation(AnnotationSegment):
 
     Most values are read from sensor and sigan calibrations,
     expected to exist in a ``measurement_result`` dictionary.
-    The sensor and sigan calibration parameters are required.
+    No parameters are required.
 
     Refer to the documentation of the ``ntia-sensor`` extension of
     SigMF for more information.
@@ -19,12 +19,12 @@ class CalibrationAnnotation(AnnotationSegment):
     :param sigan_cal: Sigan calibration result, likely stored
         in the ``measurement_result`` dictionary. This should contain
         keys: ``gain_sigan``, ``noise_figure_sigan``, ``1db_compression_sigan``,
-        and ``enbw_sigan``.
+        and ``enbw_sigan``. Any missing keys will be skipped.
     :param sensor_cal: Sensor calibration result, likely stored
         in the ``measurement_result`` dictionary. This should contain
         keys: ``gain_preselector``, ``noise_figure_sensor``, ``1db_compression_sensor``,
         ``enbw_sensor``, and ``gain_sensor``. Optionally, it can also include
-        a ``temperature`` key.
+        a ``temperature`` key. Any missing keys will be skipped.
     :param mean_noise_power_sensor: Mean noise power density of the sensor.
     :param mean_noise_power_units: The units of ``mean_noise_power_sensor``.
     :param mean_noise_power_reference: Reference point for ``mean_noise_power_sensor``,
@@ -40,20 +40,27 @@ class CalibrationAnnotation(AnnotationSegment):
     def __post_init__(self):
         super().__post_init__()
         # Load values from sensor and sigan calibrations
-        self.gain_sigan = self.sigan_cal["gain_sigan"]
-        self.noise_figure_sigan = self.sigan_cal["noise_figure_sigan"]
-        self.compression_point_sigan = self.sigan_cal["1db_compression_sigan"]
-        self.enbw_sigan = self.sigan_cal["enbw_sigan"]
-        self.gain_preselector = self.sensor_cal["gain_preselector"]
-        self.noise_figure_sensor = self.sensor_cal["noise_figure_sensor"]
-        self.compression_point_sensor = self.sensor_cal["1db_compression_sensor"]
-        self.enbw_sensor = self.sensor_cal["enbw_sensor"]
-        if "temperature" in self.sensor_cal:
-            self.temperature = self.sensor_cal["temperature"]
-        else:
-            self.temperature = None
+        self.gain_sigan = self.get_cal_value_if_exists(self.sigan_cal, "gain_sigan")
+        self.noise_figure_sigan = self.get_cal_value_if_exists(
+            self.sigan_cal, "noise_figure_sigan"
+        )
+        self.compression_point_sigan = self.get_cal_value_if_exists(
+            self.sigan_cal, "1db_compression_sigan"
+        )
+        self.enbw_sigan = self.get_cal_value_if_exists(self.sigan_cal, "enbw_sigan")
+        self.gain_preselector = self.get_cal_value_if_exists(
+            self.sensor_cal, "gain_preselector"
+        )
+        self.noise_figure_sensor = self.get_cal_value_if_exists(
+            self.sensor_cal, "noise_figure_sensor"
+        )
+        self.compression_point_sensor = self.get_cal_value_if_exists(
+            self.sensor_cal, "1db_compression_sensor"
+        )
+        self.enbw_sensor = self.get_cal_value_if_exists(self.sensor_cal, "enbw_sensor")
+        self.temperature = self.get_cal_value_if_exists(self.sensor_cal, "temperature")
         # Additional key gain_sensor is not in SigMF ntia-sensor spec but is included
-        self.gain_sensor = self.sensor_cal["gain_sensor"]
+        self.gain_sensor = self.get_cal_value_if_exists(self.sensor_cal, "gain_sensor")
         # Define SigMF key names
         self.sigmf_keys.update(
             {
@@ -74,3 +81,12 @@ class CalibrationAnnotation(AnnotationSegment):
         )
         # Create annotation segment
         super().create_annotation_segment()
+
+    @staticmethod
+    def get_cal_value_if_exists(cal_dict: dict, key_name: str) -> Any:
+        try:
+            value = cal_dict[key_name]
+            return value
+        except KeyError:
+            # create_annotation_segment skips None values
+            return None
