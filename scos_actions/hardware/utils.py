2c2b43f6dc7e1fc17c529602b35e9dba709256aa
dboulware <dboulware@ntia.doc.gov>
2022-09-06 16:12:06 -0600

    Update power_cycle_sigan docstring and when to raise exception.

--- scos_actions/hardware/utils.py
+++ scos_actions/hardware/utils.py
@@ -14,21 +14,23 @@ def power_cycle_sigan():
     Performs a hard power cycle of the signal analyzer. This method requires power to the signal analyzer is
     controlled by a Web_Relay (see https://www.github.com/ntia/Preselector) and that the switch id of that
     switch is specified in scos-sensor settings as SIGAN_POWER_SWITCH and the sequence of states is specified as
-    a comma delimited list of states in SIGAN_POWER_CYCLE_STATES. This method will raise Excwptions if the nn
+    a comma delimited list of states in SIGAN_POWER_CYCLE_STATES. This method will raise HardwareConfigurationException
+    if there are no switches configured, the switch specified to control power to the signal analyzer doesn't exist, or
+    if either SIGAN_POWER_SWITCH or SIGAN_POWER_CYCLE_STATES are not set.
     """
     if switches is None:
-        logger.debug("SWITCHES IS NONE")
-    else:
-        logger.debug(f"hardware utils has {len(switches)}")
-        for key, value in switches.items():
-            logger.debug(f"{key}: {value}")
+        raise HardwareConfigurationException(
+            "No switches are configured. Unable to power cycle signal analyzer "
+        )
 
     if SIGAN_POWER_SWITCH and SIGAN_POWER_CYCLE_STATES:
         logger.debug(f"searching for {SIGAN_POWER_SWITCH}")
         if SIGAN_POWER_SWITCH in switches:
             power_switch = switches[SIGAN_POWER_SWITCH]
             if SIGAN_POWER_CYCLE_STATES is None:
-                raise Exception("SIGAN_POWER_CYCLE_STATES not specified in settings")
+                raise HardwareConfigurationException(
+                    "SIGAN_POWER_CYCLE_STATES not specified in settings"
+                )
             else:
                 states = SIGAN_POWER_CYCLE_STATES.split(",")
                 for state in states:
@@ -39,6 +41,6 @@ def power_cycle_sigan():
                 f"Switch {SIGAN_POWER_SWITCH} does not exist. Unable to restart signal analyzer"
             )
     else:
-        logger.error(
+        raise HardwareConfigurationException(
             "Call to power cycle sigan, but no power switch or power cycle states specified "
         )
