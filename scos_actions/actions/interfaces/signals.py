60d910b474fe67f0c357ee5fce3bb2e5f04f40de
dboulware <dboulware@ntia.doc.gov>
2022-09-06 16:26:15 -0600

    Add register_action and register_signal_analyzer signals.

--- scos_actions/actions/interfaces/signals.py
+++ scos_actions/actions/interfaces/signals.py
@@ -11,3 +11,9 @@ monitor_action_completed = Signal()
 
 # Provides argument: 'component'
 register_component_with_status = Signal()
+
+# Provides argument 'sigan'
+register_signal_analyzer = Signal()
+
+# Provides argument 'action'
+register_action = Signal()
