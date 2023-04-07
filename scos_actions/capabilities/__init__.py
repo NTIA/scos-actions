31c3199d46dd44ca05d38854955ed6bd4f3bbeb5
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-07 12:35:52 -0400

    add sensor definition file hash

--- scos_actions/capabilities/__init__.py
+++ scos_actions/capabilities/__init__.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 
 from scos_actions import utils
@@ -5,10 +6,15 @@ from scos_actions.settings import FQDN, SENSOR_DEFINITION_FILE
 
 logger = logging.getLogger(__name__)
 capabilities = {}
+SENSOR_DEFINITION_HASH = None
 
 logger.info(f"Loading {SENSOR_DEFINITION_FILE}")
 try:
     capabilities["sensor"] = utils.load_from_json(SENSOR_DEFINITION_FILE)
+    # Generate sensor definition file hash (SHA 512)
+    with open(SENSOR_DEFINITION_FILE, "rb") as f:
+        digest = hashlib.file_digest(f, "sha512")
+    SENSOR_DEFINITION_HASH = digest.hexdigest()
 except Exception as e:
     logger.warning(
         f"Failed to load sensor definition file: {SENSOR_DEFINITION_FILE}"
