eae7920166045c69cecd6a79b82ba8e1b5061e34
Anthony Romaniello <aromaniello@ntia.gov>
2023-04-07 15:58:52 -0400

    debug sensor definition hash

--- scos_actions/capabilities/__init__.py
+++ scos_actions/capabilities/__init__.py
@@ -11,16 +11,24 @@ SENSOR_DEFINITION_HASH = None
 logger.info(f"Loading {SENSOR_DEFINITION_FILE}")
 try:
     capabilities["sensor"] = utils.load_from_json(SENSOR_DEFINITION_FILE)
-    # Generate sensor definition file hash (SHA 512)
-    with open(SENSOR_DEFINITION_FILE, "rb") as f:
-        digest = hashlib.file_digest(f, "sha512")
-    SENSOR_DEFINITION_HASH = digest.hexdigest()
 except Exception as e:
     logger.warning(
         f"Failed to load sensor definition file: {SENSOR_DEFINITION_FILE}"
         + "\nAn empty sensor definition will be used"
     )
     capabilities["sensor"] = {}
+
+if capabilities["sensor"]:
+    # Generate sensor definition file hash (SHA 512)
+    try:
+        with open(SENSOR_DEFINITION_FILE, "rb") as f:
+            digest = hashlib.file_digest(f, "sha512")
+        SENSOR_DEFINITION_HASH = digest.hexdigest()
+        logger.debug("Generated sensor definition hash")
+    except Exception as e:
+        logger.error(f"Unable to generate sensor definition hash")
+        # SENSOR_DEFINITION_HASH is None, do not raise Exception
+        logger.debug(e)
 
 if FQDN:
     capabilities["sensor"]["id"] = FQDN
