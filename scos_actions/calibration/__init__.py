b548321d5263fab040cf28495d8a6c8a87306487
Doug Boulware <dboulware@ntia.doc.gov>
2023-12-18 08:28:35 -0700

    run pre-commit.

--- scos_actions/calibration/__init__.py
+++ scos_actions/calibration/__init__.py
@@ -1,10 +1,12 @@
 import logging
 from os import path
+
 from scos_actions.calibration.calibration import Calibration, load_from_json
 from scos_actions.settings import SENSOR_CALIBRATION_FILE, SIGAN_CALIBRATION_FILE
 
 logger = logging.getLogger(__name__)
-DEFAULT_CALIBRATION_FILE="no_calibration.json"
+DEFAULT_CALIBRATION_FILE = "no_calibration.json"
+
 
 def get_sigan_calibration(sigan_cal_file: str) -> Calibration:
     """
@@ -39,16 +41,23 @@ def get_sensor_calibration(sensor_cal_file: str) -> Calibration:
         logger.exception("Unable to load sensor calibration data, reverting to none")
     return sensor_cal
 
+
 def check_for_default_calibration(cal_file_path: str, cal_type: str):
     cal_file_name = path.basename(cal_file_path)
     if cal_file_name == DEFAULT_CALIBRATION_FILE:
-        logger.warning(f"***************LOADING DEFAULT {cal_type} CALIBRATION***************")
+        logger.warning(
+            f"***************LOADING DEFAULT {cal_type} CALIBRATION***************"
+        )
+
 
 sensor_calibration = None
 if SENSOR_CALIBRATION_FILE is None:
     logger.warning("Sensor calibration file is None. Not loading calibration file.")
 elif not path.exists(SENSOR_CALIBRATION_FILE):
-    logger.warning(SENSOR_CALIBRATION_FILE + " does not exist. Not loading sensor calibration file.")
+    logger.warning(
+        SENSOR_CALIBRATION_FILE
+        + " does not exist. Not loading sensor calibration file."
+    )
 else:
     logger.debug(f"Loading sensor cal file: {SENSOR_CALIBRATION_FILE}")
     sensor_calibration = get_sensor_calibration(SENSOR_CALIBRATION_FILE)
@@ -57,7 +66,9 @@ sigan_calibration = None
 if SIGAN_CALIBRATION_FILE is None:
     logger.warning("Sigan calibration  file is None. Not loading calibration file.")
 elif not path.exists(SIGAN_CALIBRATION_FILE):
-    logger.warning(SIGAN_CALIBRATION_FILE + " does not exist. Not loading sigan calibration file.")
+    logger.warning(
+        SIGAN_CALIBRATION_FILE + " does not exist. Not loading sigan calibration file."
+    )
 else:
     logger.debug(f"Loading sigan cal file: {SIGAN_CALIBRATION_FILE}")
     sigan_calibration = get_sigan_calibration(SIGAN_CALIBRATION_FILE)
