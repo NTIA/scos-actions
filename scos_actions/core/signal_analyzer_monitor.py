8a3b2c220e31e53b0d38d39ef43fa09a2236c305
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-08 21:19:49 -0700

    typo fix.

--- scos_actions/core/signal_analyzer_monitor.py
+++ scos_actions/core/signal_analyzer_monitor.py
@@ -6,12 +6,12 @@ logger = logging.getLogger(__name__)
 class SignalAnalyzerMonitor:
     def __init__(self):
         logger.debug("Initializing Signal Analyzer Monitor")
-        self.signal_analyzer = None
+        self._signal_analyzer = None
 
     def register_signal_analyzer(self, sigan):
         logger.debug(f"Setting Signal Analyzer to {sigan}")
-        self.signal_analyzer = sigan
+        self._signal_analyzer = sigan
 
     @property
     def signal_analyzer(self):
-        return self.signal_analyzer
+        return self._signal_analyzer
