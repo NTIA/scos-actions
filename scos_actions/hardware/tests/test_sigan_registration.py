027e23a35b5359163f7e97e4c6aebd88fc6d5113
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-11 16:43:15 -0700

    remove old tests and fix test_status_handler.

--- scos_actions/hardware/tests/test_sigan_registration.py
+++
@@ -1,9 +0,0 @@
-from scos_actions.core import signal_analyzer_monitor
-from scos_actions.hardware.mocks.mock_sigan import MockSignalAnalyzer
-from scos_actions.signals import register_signal_analyzer
-
-
-def test_sigan_registration():
-    sigan = MockSignalAnalyzer()
-    register_signal_analyzer.send(__name__, signal_analyzer=sigan)
-    assert signal_analyzer_monitor.signal_analyzer == sigan
--- scos_actions/hardware/tests/test_status_handler.py
+++ scos_actions/hardware/tests/test_status_handler.py
@@ -1,4 +1,4 @@
-from scos_actions.core import status_registrar
+from scos_actions.core import status_monitor
 from scos_actions.hardware.mocks.mock_sigan import MockSignalAnalyzer
 from scos_actions.signals import register_component_with_status
 
@@ -6,4 +6,4 @@ from scos_actions.signals import register_component_with_status
 def test_status_handler():
     mock_sigan = MockSignalAnalyzer()
     register_component_with_status.send(__name__, component=mock_sigan)
-    status_registrar.status_components[0] == mock_sigan
+    status_monitor.status_components[0] == mock_sigan
