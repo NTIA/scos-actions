3c020d6ebf2b9fad4e225757d6492576c3ca2792
Doug Boulware <dboulware@ntia.doc.gov>
2024-01-15 14:49:58 -0700

    Use SCOS_SENSOR_GIT_TAG from environment.

--- scos_actions/settings.py
+++ scos_actions/settings.py
@@ -37,6 +37,7 @@ else:
     logger.debug(f"SCOS_ACTIONS: SENSOR_CALIBRATION_FILE: {SENSOR_CALIBRATION_FILE}")
 
 SWITCH_CONFIGS_DIR = env("SWITCH_CONFIGS_DIR", default=None)
+SCOS_SENSOR_GIT_TAG = env("SCOS_SENSOR_GIT_TAG", default="unknown")
 if not settings.configured:
     PRESELECTOR_CONFIG_FILE = None
     SENSOR_DEFINITION_FILE = None
@@ -46,25 +47,12 @@ if not settings.configured:
     SIGAN_POWER_CYCLE_STATES = env("SIGAN_POWER_CYCLE_STATES", default=None)
     SIGAN_POWER_SWITCH = env("SIGAN_POWER_SWITCH", default=None)
     MOCK_SIGAN = env("MOCK_SIGAN", default=None)
-    SCOS_SENSOR_GIT_TAG = env("SCOS_SENSOR_GIT_TAG", default="unknown")
+
 else:
     MOCK_SIGAN = settings.MOCK_SIGAN
     RUNNING_TESTS = settings.RUNNING_TESTS
     SENSOR_DEFINITION_FILE = Path(settings.SENSOR_DEFINITION_FILE)
     FQDN = settings.FQDN
-    SCOS_SENSOR_GIT_TAG = settings.SCOS_SENSOR_GIT_TAG
-    # if settings.PRESELECTOR_CONFIG:
-    #     PRESELECTOR_CONFIG_FILE = settings.PRESELECTOR_CONFIG
-    # else:
-    #     PRESELECTOR_CONFIG_FILE = None
-    #
-    # if settings.PRESELECTOR_MODULE and settings.PRESELECTOR_CLASS:
-    #     PRESELECTOR_MODULE = settings.PRESELECTOR_MODULE
-    #     PRESELECTOR_CLASS = settings.PRESELECTOR_CLASS
-    # else:
-    #     PRESELECTOR_MODULE = "its_preselector.web_relay_preselector"
-    #     PRESELECTOR_CLASS = "WebRelayPreselector"
-
     SIGAN_POWER_SWITCH = None
     SIGAN_POWER_CYCLE_STATES = None
     if hasattr(settings, "SIGAN_POWER_SWITCH"):
