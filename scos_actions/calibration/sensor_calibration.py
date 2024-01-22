fc2d450b6adba1a494023c9918a56484fb6f9d76
Anthony Romaniello <aromaniello@ntia.gov>
2024-01-22 16:07:11 -0500

    remove unused imports

--- scos_actions/calibration/sensor_calibration.py
+++ scos_actions/calibration/sensor_calibration.py
@@ -1,7 +1,5 @@
-import json
 import logging
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Dict, List, Union
 
 from scos_actions.calibration.interfaces.calibration import Calibration
