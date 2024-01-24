eba790a42c81ecf6cd075c7a8cd7b17c3938744d
Anthony Romaniello <aromaniello@ntia.gov>
2024-01-24 13:33:34 -0500

    generalize differential calibration module docstring

--- scos_actions/calibration/differential_calibration.py
+++ scos_actions/calibration/differential_calibration.py
@@ -1,20 +1,20 @@
 """
 Dataclass implementation for "differential calibration" handling.
 
-A differential calibration provides loss values at different frequencies
-which represent excess loss between the calibration terminal and the antenna
-port. At present, this is measured manually using a calibration probe consisting
-of a calibrated noise source and a programmable attenuator.
+A differential calibration provides loss values which represent excess loss
+between the `SensorCalibration` reference point and another reference point.
+A typical usage would be for calibrating out measured cable losses which exist
+between the antenna and the Y-factor calibration terminal. At present, this is
+measured manually using a calibration probe consisting of a calibrated noise
+source and a programmable attenuator.
 
 The ``reference_point`` top-level key defines the point to which measurements
 are referenced after using the correction factors included in the file.
 
 The ``calibration_data`` entries are expected to include these correction factors,
-with the key name ``"loss"`` and values in decibels (dB). These correction factors
-represent the differential loss between the calibration terminal used by onboard
-``SensorCalibration`` results and the reference point defined by ``reference_point``.
-A positive value of ``"loss"`` indicates a LOSS going FROM ``reference_point`` TO
-the calibration terminal used by the ``SensorCalibration``.
+with the key name ``"loss"`` and values in decibels (dB). A positive value of
+``"loss"`` indicates a LOSS going FROM ``reference_point`` TO the calibration
+terminal used by the ``SensorCalibration``.
 """
 
 from dataclasses import dataclass
