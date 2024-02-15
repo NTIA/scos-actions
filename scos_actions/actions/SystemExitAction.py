d8b9fc2b4774499519adec8f835727d0cee8cf9b
Doug Boulware <dboulware@ntia.doc.gov>
2024-02-15 12:54:37 -0700

    add SystemExitAction

---
+++ scos_actions/actions/SystemExitAction.py
@@ -0,0 +1,32 @@
+"""A simple action that raises SystemExit"""
+
+import logging
+from typing import Optional
+
+from scos_actions.actions.interfaces.action import Action
+from scos_actions.hardware.sensor import Sensor
+
+logger = logging.getLogger(__name__)
+
+LOGLVL_INFO = 20
+LOGLVL_ERROR = 40
+
+
+class SystemExitAction(Action):
+    """Raise a SystemExit".
+
+    This is useful for testing and debugging.
+
+    """
+
+    def __init__(self, loglvl=LOGLVL_INFO):
+        super().__init__(parameters={"name": "SystemExit"})
+        self.loglvl = loglvl
+
+    def __call__(self, sensor: Optional[Sensor], schedule_entry: dict, task_id: int):
+        msg = "Raising runtime exception {name}/{tid}"
+        schedule_entry_name = schedule_entry["name"]
+        logger.log(
+            level=self.loglvl, msg=msg.format(name=schedule_entry_name, tid=task_id)
+        )
+        raise SystemExit("SystemExit produced by SystemExitAction. ")
