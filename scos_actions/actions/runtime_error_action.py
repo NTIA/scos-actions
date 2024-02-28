6cc1a71b551ead0e2bd9fec26c4a98b5429c8647
Doug Boulware <dboulware@ntia.doc.gov>
2024-02-28 09:51:17 -0700

    Rename RuntimeErrorAction and cleanup.

--- scos_actions/actions/runtime_error_action.py
+++ scos_actions/actions/runtime_error_action.py
@@ -1,4 +1,4 @@
-"""A simple action that raises SystemExit"""
+"""A simple example action that raises a RuntimeError."""
 
 import logging
 from typing import Optional
@@ -9,18 +9,18 @@ from scos_actions.hardware.sensor import Sensor
 logger = logging.getLogger(__name__)
 
 
-class SystemExitAction(Action):
+class RuntimeErrorAction(Action):
     """
-    Raise a SystemExit.
+    Raise a runtime error.
     This is useful for testing and debugging.
 
     """
 
     def __init__(self):
-        super().__init__(parameters={"name": "SystemExitAction"})
+        super().__init__(parameters={"name": "RuntimeErrorAction"})
 
     def __call__(self, sensor: Optional[Sensor], schedule_entry: dict, task_id: int):
-        msg = "Raising SystemExit {name}/{tid}"
+        msg = "Raising runtime exception {name}/{tid}"
         schedule_entry_name = schedule_entry["name"]
         logger.log(msg=msg.format(name=schedule_entry_name, tid=task_id))
-        raise SystemExit("SystemExit produced by SystemExitAction. ")
+        raise RuntimeError("RuntimeError from RuntimeErrorAction")
