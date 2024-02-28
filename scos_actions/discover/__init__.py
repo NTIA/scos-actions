3d45995bf5e1cd24499f5781b618681edaeed354
Doug Boulware <dboulware@ntia.doc.gov>
2024-02-28 10:12:00 -0700

    Delete runtime_exception.py

--- scos_actions/discover/__init__.py
+++ scos_actions/discover/__init__.py
@@ -1,7 +1,7 @@
 from scos_actions.actions import action_classes
 from scos_actions.actions.logger import Logger
 from scos_actions.actions.monitor_sigan import MonitorSignalAnalyzer
-from scos_actions.actions.runtime_exception import RuntimeException
+from scos_actions.actions.runtime_error_action import RuntimeErrorAction
 from scos_actions.actions.sync_gps import SyncGps
 from scos_actions.actions.system_exit_action import SystemExitAction
 from scos_actions.discover.yaml import load_from_yaml
@@ -9,7 +9,7 @@ from scos_actions.settings import ACTION_DEFINITIONS_DIR
 
 actions = {
     "logger": Logger(),
-    "RuntimeException": RuntimeException(),
+    "RuntimeError": RuntimeErrorAction(),
     "SystemExit": SystemExitAction(),
 }
 test_actions = {
@@ -18,6 +18,8 @@ test_actions = {
         parameters={"name": "test_monitor_sigan"}
     ),
     "logger": Logger(),
+    "RuntimeError": RuntimeErrorAction(),
+    "SystemExit": SystemExitAction(),
 }
 
 
