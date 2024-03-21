Restore type hint in acquire_sea_data_product. Fix duration ms computation. Remove commented out code. Reuse create_capture_segment in stepped frequency IQ action. Use frequency from measurement_result in MeasurementAction. Fix description of the calibration data that will be used in the docstring in Sensor acquire_time_domain_samples.

scos_actions/actions/acquire_sea_data_product.py
@@ -425,7 +425,7 @@ class IQProcessor:
         ]
         del params
 
-    def run(self, iqdata: np.ndarray):
+    def run(self, iqdata: np.ndarray) -> list:
         """
         Filter the input IQ data and concurrently compute FFT, PVT, PFP, and APD results.
 