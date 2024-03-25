ae416030e409b3946bed725ae07680e4ed428f6e
Anthony Romaniello <aromaniello@ntia.gov>
2024-03-25 17:15:10 -0400

    Clarify filter_by_parameter debug message

--- scos_actions/calibration/utils.py
+++ scos_actions/calibration/utils.py
@@ -72,6 +72,7 @@ def filter_by_parameter(calibrations: dict, value: Union[float, int, bool]) -> d
             f"Could not locate calibration data at {value}"
             + f"\nAttempted lookup using key '{str(value).lower()}'"
             + f"{f' and {float(value)}' if isinstance(value, float) and value.is_integer() else ''}"
+            + f"{f' and {int(value)}' if isinstance(value, float) and value.is_integer() else ''}"
             + f"\nUsing calibration data: {calibrations}"
         )
         raise CalibrationEntryMissingException(msg)
