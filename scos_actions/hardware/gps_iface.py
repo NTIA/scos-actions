d31020241dc924b8eaf459fa854824d30a5f11e0
Justin Haze <jhaze@ntia.gov>
2024-04-05 12:33:45 -0600

    fix circular import

--- scos_actions/hardware/gps_iface.py
+++ scos_actions/hardware/gps_iface.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 
-from scos_actions.hardware.sensor import Sensor
-
 
 class GPSInterface(ABC):
     @abstractmethod
-    def get_location(self, sensor : Sensor, timeout_s : float=1):
+    def get_location(
+        self, sensor: "scos_actions.hardware.sensor.Sensor", timeout_s: float = 1
+    ):
         pass
 
     @abstractmethod
-    def get_gps_time(self, sensor : Sensor):
+    def get_gps_time(self, sensor: "scos_actions.hardware.sensor.Sensor"):
         pass
