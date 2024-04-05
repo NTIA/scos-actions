93a32d0419bc311ee3660844d4c94cd5568fce84
Justin Haze <jhaze@ntia.gov>
2024-04-05 11:20:49 -0600

    change GPS methods to use sensor, add type hints

--- scos_actions/hardware/gps_iface.py
+++ scos_actions/hardware/gps_iface.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+from scos_actions.hardware.sensor import Sensor
+
 
 class GPSInterface(ABC):
     @abstractmethod
-    def get_location(self, sigan, timeout_s=1):
+    def get_location(self, sensor : Sensor, timeout_s : float=1):
         pass
 
     @abstractmethod
-    def get_gps_time(self, sigan):
+    def get_gps_time(self, sensor : Sensor):
         pass
--- scos_actions/hardware/mocks/mock_gps.py
+++ scos_actions/hardware/mocks/mock_gps.py
@@ -8,10 +8,10 @@ logger = logging.getLogger(__name__)
 
 class MockGPS(GPSInterface):
 
-    def get_location(self, sigan, timeout_s=1):
+    def get_location(self, sensor, timeout_s=1):
         logger.warning("Using mock GPS!")
         return 39.995118, -105.261572, 1651.0
 
-    def get_gps_time(self, sigan):
+    def get_gps_time(self, sensor):
         logger.warning("Using mock GPS!")
         return datetime.now()
