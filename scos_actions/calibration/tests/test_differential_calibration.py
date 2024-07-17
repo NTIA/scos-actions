8608b774ce79446581daf68ad278e61b59b93c9b
Anthony Romaniello <aromaniello@ntia.gov>
2024-07-17 12:39:32 -0400

    run pre-commit hooks on all files

--- scos_actions/calibration/tests/test_differential_calibration.py
+++ scos_actions/calibration/tests/test_differential_calibration.py
@@ -22,22 +22,25 @@ class TestDifferentialCalibration:
             "calibration_data": {
                 "antenna1": {3555e6: 11.5},
                 "antenna2": {3555e6: 21.5},
-            }
+            },
         }
         self.valid_file_path = tmp_path / "sample_diff_cal.json"
-        self.valid_file_path_multiple_antenna = tmp_path / "sample_diff_cal_multiple_antenna.json"
+        self.valid_file_path_multiple_antenna = (
+            tmp_path / "sample_diff_cal_multiple_antenna.json"
+        )
         self.invalid_file_path = tmp_path / "sample_diff_cal_invalid.json"
 
         self.sample_diff_cal = DifferentialCalibration(
             file_path=self.valid_file_path, **dict_to_json
         )
         self.sample_diff_cal_multiple_antenna = DifferentialCalibration(
-            file_path=self.valid_file_path_multiple_antenna, **dict_to_json_multiple_antenna
+            file_path=self.valid_file_path_multiple_antenna,
+            **dict_to_json_multiple_antenna,
         )
 
         with open(self.valid_file_path, "w") as f:
             f.write(json.dumps(dict_to_json))
-        
+
         with open(self.valid_file_path_multiple_antenna, "w") as f:
             f.write(json.dumps(dict_to_json_multiple_antenna))
 
@@ -50,7 +53,9 @@ class TestDifferentialCalibration:
         """Check from_json functionality with valid and invalid dummy data."""
         diff_cal = DifferentialCalibration.from_json(self.valid_file_path)
         assert diff_cal == self.sample_diff_cal
-        diff_cal_multiple_antenna = DifferentialCalibration.from_json(self.valid_file_path_multiple_antenna)
+        diff_cal_multiple_antenna = DifferentialCalibration.from_json(
+            self.valid_file_path_multiple_antenna
+        )
         assert diff_cal_multiple_antenna == self.sample_diff_cal_multiple_antenna
         with pytest.raises(Exception):
             _ = DifferentialCalibration.from_json(self.invalid_file_path)
